741b27d7e6b1e614f9d978e7df3d518780b2fd15
“ <lovelyWu98k@gamil.com>
2019-05-27 13:54:33 +0800

    dynamic

--- SpringBoot/mybatis-plus/src/main/java/com/simple/spring/boot/mapper/UserMapper.java
+++ SpringBoot/mybatis-plus/src/main/java/com/simple/spring/boot/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package com.simple.spring.boot.mapper;
 
+import com.baomidou.dynamic.datasource.annotation.DS;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.simple.spring.boot.entities.User;
 import org.apache.ibatis.annotations.Param;
@@ -11,6 +12,7 @@ import java.util.Map;
  * @author: SimpleWu
  * @date: 2019/5/25
  */
+@DS("slave_1")
 public interface UserMapper extends BaseMapper<User> {
 
     Map<String,Object> queryUser(@Param("USER_ID") String userId);
